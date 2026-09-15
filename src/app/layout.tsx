1bc30cfce7bbfb3a71005eb69e219a38fc8b9711
Bakya <git@webstix.com>
2026-09-15 16:38:14 +0530

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
@@ -89,6 +90,18 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${montserrat.variable}`}>
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-KS74L5S9SW"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KS74L5S9SW');
+          `}
+        </Script>
         <JsonLd data={globalJsonLd()} />
         <Header />
         <main style={{ flexGrow: 1 }}>{children}</main>
